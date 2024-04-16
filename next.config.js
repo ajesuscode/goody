173d11609ea305d56de89e895d98d854eceb1c1a
ajesuscode <ajesusnode@gmail.com>
2024-04-16 15:03:04 +0200

    fix: next cofig

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    output: 'standalone',
+    experimental: {
+        serverActions: {
+            allowedOrigins: ['103.241.67.124:3000'],
+        },
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
